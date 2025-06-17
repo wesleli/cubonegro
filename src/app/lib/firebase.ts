e15d1501e6d94a0c36aa16a9043ee524888cdb97
Wesley Silva <wesley.ppssilva@gmail.com>
2025-06-17 02:44:38 -0300

    postando codigo no repositorio

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -10,13 +10,13 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDQTM3FzXla4BZVoA39Eb1TfFRK702IKb8",
-  authDomain: "cubo-5e1ab.firebaseapp.com",
-  projectId: "cubo-5e1ab",
-  storageBucket: "cubo-5e1ab.firebasestorage.app",
-  messagingSenderId: "536689579528",
-  appId: "1:536689579528:web:cdc8bdfbc31570ef0fe3bd",
-  measurementId: "G-WYCZMWW7XH"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0]!;
